e5140cb6e1f2a0c014c55172de658ed389b233c0
Damian1234523 <dpistorius@posteo.de>
2017-04-04 11:15:20 +0200

    SourceManager now filters the commandsection in a seperat list

--- PicSim/SourceManager.cs
+++ PicSim/SourceManager.cs
@@ -10,12 +10,21 @@ namespace PicSim
     {
         List<string> sourceComplete = new List<string>();
         List<int> args1 = new List<int>();
-        List<int> args2 = new List<int>();
 
 
         public void FillSource(List<string> l)
         {
             sourceComplete = l;
+            foreach (string line in sourceComplete)
+            {
+                Console.WriteLine(line);
+                string sArg1 = line.Substring(5, 4);
+
+                if (sArg1 != "    ")
+                {
+                    args1.Add(Convert.ToInt32(sArg1, 16));
+                }
+            }
         }
 
         public void ResetSource()
